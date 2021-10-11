67fc1b102af584735f70c258a4153d781782cdc7
KaliWare101 <92335114+KaliWare101@users.noreply.github.com>
2021-10-11 20:48:01 +0100

    Add files via upload

---
+++ NortsNCrosses/NortsNCrosses/Nnc.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace NortsNCrosses
+{
+    partial class Nnc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tl = new System.Windows.Forms.Button();
+            this.tm = new System.Windows.Forms.Button();
+            this.tr = new System.Windows.Forms.Button();
+            this.ml = new System.Windows.Forms.Button();
+            this.mm = new System.Windows.Forms.Button();
+            this.mr = new System.Windows.Forms.Button();
+            this.bm = new System.Windows.Forms.Button();
+            this.bl = new System.Windows.Forms.Button();
+            this.br = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkwin = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tl
+            // 
+            this.tl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tl.ForeColor = System.Drawing.Color.Cornsilk;
+            this.tl.Location = new System.Drawing.Point(6, 19);
+            this.tl.Name = "tl";
+            this.tl.Size = new System.Drawing.Size(65, 56);
+            this.tl.TabIndex = 1;
+            this.tl.UseVisualStyleBackColor = true;
+            this.tl.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tm
+            // 
+            this.tm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tm.ForeColor = System.Drawing.Color.Cornsilk;
+            this.tm.Location = new System.Drawing.Point(77, 19);
+            this.tm.Name = "tm";
+            this.tm.Size = new System.Drawing.Size(65, 56);
+            this.tm.TabIndex = 2;
+            this.tm.UseVisualStyleBackColor = true;
+            this.tm.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tr
+            // 
+            this.tr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tr.ForeColor = System.Drawing.Color.Cornsilk;
+            this.tr.Location = new System.Drawing.Point(148, 19);
+            this.tr.Name = "tr";
+            this.tr.Size = new System.Drawing.Size(65, 56);
+            this.tr.TabIndex = 3;
+            this.tr.UseVisualStyleBackColor = true;
+            this.tr.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ml
+            // 
+            this.ml.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ml.ForeColor = System.Drawing.Color.Cornsilk;
+            this.ml.Location = new System.Drawing.Point(6, 81);
+            this.ml.Name = "ml";
+            this.ml.Size = new System.Drawing.Size(65, 56);
+            this.ml.TabIndex = 4;
+            this.ml.UseVisualStyleBackColor = true;
+            this.ml.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // mm
+            // 
+            this.mm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mm.ForeColor = System.Drawing.Color.Cornsilk;
+            this.mm.Location = new System.Drawing.Point(77, 81);
+            this.mm.Name = "mm";
+            this.mm.Size = new System.Drawing.Size(65, 56);
+            this.mm.TabIndex = 5;
+            this.mm.UseVisualStyleBackColor = true;
+            this.mm.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // mr
+            // 
+            this.mr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mr.ForeColor = System.Drawing.Color.Cornsilk;
+            this.mr.Location = new System.Drawing.Point(148, 81);
+            this.mr.Name = "mr";
+            this.mr.Size = new System.Drawing.Size(65, 56);
+            this.mr.TabIndex = 6;
+            this.mr.UseVisualStyleBackColor = true;
+            this.mr.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // bm
+            // 
+            this.bm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bm.ForeColor = System.Drawing.Color.Cornsilk;
+            this.bm.Location = new System.Drawing.Point(77, 143);
+            this.bm.Name = "bm";
+            this.bm.Size = new System.Drawing.Size(65, 56);
+            this.bm.TabIndex = 7;
+            this.bm.UseVisualStyleBackColor = true;
+            this.bm.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // bl
+            // 
+            this.bl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bl.ForeColor = System.Drawing.Color.Cornsilk;
+            this.bl.Location = new System.Drawing.Point(6, 143);
+            this.bl.Name = "bl";
+            this.bl.Size = new System.Drawing.Size(65, 56);
+            this.bl.TabIndex = 8;
+            this.bl.UseVisualStyleBackColor = true;
+            this.bl.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // br
+            // 
+            this.br.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.br.ForeColor = System.Drawing.Color.Cornsilk;
+            this.br.Location = new System.Drawing.Point(148, 143);
+            this.br.Name = "br";
+            this.br.Size = new System.Drawing.Size(65, 56);
+            this.br.TabIndex = 9;
+            this.br.UseVisualStyleBackColor = true;
+            this.br.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.tl);
+            this.groupBox1.Controls.Add(this.br);
+            this.groupBox1.Controls.Add(this.ml);
+            this.groupBox1.Controls.Add(this.mr);
+            this.groupBox1.Controls.Add(this.bl);
+            this.groupBox1.Controls.Add(this.mm);
+            this.groupBox1.Controls.Add(this.tr);
+            this.groupBox1.Controls.Add(this.bm);
+            this.groupBox1.Controls.Add(this.tm);
+            this.groupBox1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 209);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Player ( X )";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // checkwin
+            // 
+            this.checkwin.Tick += new System.EventHandler(this.checkwin_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(6, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 180);
+            this.panel1.TabIndex = 12;
+            // 
+            // Nnc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(242, 254);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Nnc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Norts N Corsses 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tl;
+        private System.Windows.Forms.Button tm;
+        private System.Windows.Forms.Button tr;
+        private System.Windows.Forms.Button ml;
+        private System.Windows.Forms.Button mm;
+        private System.Windows.Forms.Button mr;
+        private System.Windows.Forms.Button bm;
+        private System.Windows.Forms.Button bl;
+        private System.Windows.Forms.Button br;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer checkwin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ NortsNCrosses/NortsNCrosses/Nnc.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NortsNCrosses
+{
+    public partial class Nnc : Form
+    {
+        public Nnc()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        void Clear()
+        {
+            tl.Text = "";
+            tm.Text = "";
+            tr.Text = "";
+            ml.Text = "";
+            mr.Text = "";
+            mm.Text = "";
+            bl.Text = "";
+            bm.Text = "";
+            br.Text = "";
+            checkwin.Stop();
+            button1.Text = "Play";
+            panel1.Show();
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (groupBox1.Text == "Player ( X )")
+                {
+                    button.Text = "X";
+                    groupBox1.Text = "Player ( O )";
+                }
+                else
+                {
+                    button.Text = "O";
+                    groupBox1.Text = "Player ( X )";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Move already taken!");
+            }
+        }
+
+        private void checkwin_Tick(object sender, EventArgs e)
+        {
+            if(tr.Text == "X" & mr.Text == "X" & br.Text == "X" || tm.Text == "X" & mm.Text == "X" & bm.Text == "X" 
+                || tl.Text == "X" & ml.Text == "X" & bl.Text == "X" || tl.Text == "X" & tm.Text == "X" & tr.Text == "X" 
+                || ml.Text == "X" & mm.Text == "X" & mr.Text == "X" || bl.Text == "X" & bm.Text == "X" & br.Text == "X"
+                || tl.Text == "X" & mm.Text == "X" & br.Text == "X" || bl.Text == "X" & mm.Text == "X" & tr.Text == "X")
+            {
+                checkwin.Stop();
+                MessageBox.Show("Player ( X )\nis the winner", "Hooray", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Clear();
+            }
+            else if(tr.Text == "O" & mr.Text == "O" & br.Text == "O" || tm.Text == "O" & mm.Text == "O" & bm.Text == "O"
+                || tl.Text == "O" & ml.Text == "O" & bl.Text == "O" || tl.Text == "O" & tm.Text == "O" & tr.Text == "O"
+                || ml.Text == "O" & mm.Text == "O" & mr.Text == "O" || bl.Text == "O" & bm.Text == "O" & br.Text == "O"
+                || tl.Text == "O" & mm.Text == "O" & br.Text == "O" || bl.Text == "O" & mm.Text == "O" & tr.Text == "O")
+            {
+                checkwin.Stop();
+                MessageBox.Show("Player ( O )\nis the winner", "Hooray", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Clear();
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (button1.Text == "Play")
+            {
+                panel1.Hide();
+                checkwin.Start();
+                button1.Text = "Stop";
+            }
+            else
+            {
+                Clear();
+            }
+        }
+    }
+}
